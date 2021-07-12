4fae86f70bb1299c7cac687aea37a2c0a32fa5c8
bite7523aszx <82069373+bite7523aszx@users.noreply.github.com>
2021-07-12 08:24:52 +0800

    1100712

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject cactus;
+    public GameObject cactusSpawnPosition;
+    public float spawnTime;
+    float timer;
+    public GameObject GameOverScene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer >= spawnTime)
+        {
+            Instantiate(cactus, cactusSpawnPosition.transform);
+            timer = 0;
+        }
+    }
+    public void GameOver()
+    {
+        Time.timeScale = 0;
+        GameOverScene.SetActive(true);
+    }
+    public void Restar()
+    {
+        SceneManager.LoadScene("Lv1");
+    }
+}
